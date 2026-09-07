1dc25b636670e4f4ed422cdef7f8b18394b67090
Martin Pettersson <martin.pettersson@outlook.com>
2026-09-08 00:18:56 +0200

    count_code

--- string2.py
+++ string2.py
@@ -14,3 +14,11 @@ def cat_dog(str):
         return True
     else:
         return False
+
+def count_code(str):
+    count = 0
+    for i in range(0, len(str)-3):
+        if str[i] == 'c' and str[i+1] == 'o' and str[i+3] == 'e':
+            count += 1
+    return count
+
