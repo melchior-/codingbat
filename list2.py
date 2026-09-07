6e48455d18397ba3a6f871a0c962061e10af42e2
Martin Pettersson <martin.pettersson@outlook.com>
2026-09-08 01:02:33 +0200

    sum13

--- list2.py
+++ list2.py
@@ -10,4 +10,27 @@ def big_diff(nums):
     lol2 = min(nums)
     return lol1-lol2
 
-print(big_diff([10,3,5,6]))
+def centered_average(nums):
+    m = max(nums)
+    s = min(nums)
+    sum = 0
+    for num in nums:
+        sum += num
+    sum -= m
+    sum -= s
+    return int(sum/(len(nums)-2))
+
+def sum13(nums):
+    if len(nums) == 0:
+        return 0
+    sum = 0
+    prev13 = False
+    for num in nums:
+        if prev13:
+            prev13 = False
+            continue
+        if (num == 13):
+            prev13 = True
+            continue
+        sum += num
+    return sum
