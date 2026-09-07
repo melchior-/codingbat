95595c7fadee97a3f149abf865c95f5b071dbea9
Martin Pettersson <martin.pettersson@outlook.com>
2026-09-08 01:07:32 +0200

    has22

--- list2.py
+++ list2.py
@@ -48,3 +48,9 @@ def sum67(nums):
             continue
         sum += num
     return sum
+
+def has22(nums):
+    for i in range(0, len(nums)-1):
+        if nums[i] == 2 and nums[i+1] == 2:
+            return True
+    return False
