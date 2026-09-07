f455645c6c420a909b57b57535430151cc8e1739
Martin Pettersson <martin.pettersson@outlook.com>
2026-09-08 00:52:15 +0200

    big_diff

--- list2.py
+++ list2.py
@@ -4,3 +4,10 @@ def count_evens(nums):
         if num % 2 == 0:
             count += 1
     return count
+
+def big_diff(nums):
+    lol1 = max(nums)
+    lol2 = min(nums)
+    return lol1-lol2
+
+print(big_diff([10,3,5,6]))
