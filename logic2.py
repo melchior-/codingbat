e2ce8fa2ade74e685d37457170fee37533d8ea48
Martin Pettersson <martin.pettersson@outlook.com>
2026-09-07 23:11:31 +0200

    no_teen_sum

--- logic2.py
+++ logic2.py
@@ -36,3 +36,15 @@ def lucky_sum(a, b, c):
     if c == 13:
         return a + b
     return a + b + c
+
+def no_teen_sum(a, b, c):
+    return fix_teen(a) + fix_teen(b) + fix_teen(c)
+
+def fix_teen(n):
+    if n == 15:
+        return 15
+    if n == 16:
+        return 16
+    if n >= 13 and n <= 19:
+        return 0
+    return n
