4ad948d485db5002dc4b5c068629318a005b3b3e
Martin Pettersson <martin.pettersson@outlook.com>
2026-09-07 23:03:26 +0200

    lucky_sum

--- logic2.py
+++ logic2.py
@@ -27,3 +27,12 @@ def lone_sum(a, b, c):
     if (c == a or c == b):
         sum -= c
     return sum
+
+def lucky_sum(a, b, c):
+    if a == 13:
+        return 0
+    if b == 13:
+        return a
+    if c == 13:
+        return a + b
+    return a + b + c
