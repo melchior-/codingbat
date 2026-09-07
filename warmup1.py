bb9a71512354ccc67e3331763bea38d2c700e83e
Martin Pettersson <martin.pettersson@outlook.com>
2026-09-07 13:58:54 +0200

    monkey_trouble

--- warmup1.py
+++ warmup1.py
@@ -3,3 +3,9 @@ def sleep_in(weekday, vacation):
         return True
     else:
         return False
+
+def monkey_trouble(a_smile, b_smile):
+    if (a_smile and b_smile) or (not a_smile and not b_smile):
+        return True
+    else:
+        return False
