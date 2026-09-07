96bec69895a9c902b35791420672670380e04f0b
Martin Pettersson <martin.pettersson@outlook.com>
2026-09-07 15:17:06 +0200

    string_splosion

--- warmup2.py
+++ warmup2.py
@@ -14,3 +14,11 @@ def string_bits(str):
     for i in range(0, len(str), 2):
         return_str += str[i]
     return return_str
+
+def string_splosion(str):
+    a = len(str)
+    return_str = ""
+    for i in range(0, a):
+        substr = str[0:i]
+        return_str += substr
+    return return_str + str
